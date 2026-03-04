98a1b55344987ab8fa1064d3b268112909d615da
dhanesvara481 <abhisina08@gmail.com>
2026-03-04 21:36:41 +0700

    DB Notifikasi utf8mb4

--- database/migrations/2025_12_05_153158_create_notifikasi_table.php
+++ database/migrations/2025_12_05_153158_create_notifikasi_table.php
@@ -3,12 +3,10 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('notifikasi', function (Blueprint $table) {
@@ -20,11 +18,12 @@ return new class extends Migration
             $table->string('judul_notif', 30);
             $table->timestamps();
         });
+
+        // Set seluruh tabel ke utf8mb4 supaya support emoji & karakter 4-byte
+        DB::statement("ALTER TABLE `notifikasi`
+            CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci");
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('notifikasi');
