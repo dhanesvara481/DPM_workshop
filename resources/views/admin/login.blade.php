53df2e313c93e6ccf6286deadb92493e7ad94f87
dhanesvara481 <abhisina08@gmail.com>
2026-03-07 21:39:19 +0700

    edit tulisan dikit

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login — DPW Workshop</title>
+    <title>Login — DPM Workshop</title>
 
     @vite('resources/js/app.js')
 </head>
